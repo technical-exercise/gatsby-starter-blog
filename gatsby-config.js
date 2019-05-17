0621b1e001513c0386e9a33013ff97246237e654
Ivan <me@example.com>
2019-05-17 12:32:33 -0500

    Copy and paste

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Process Blog`,
+    author: `Ivan`,
+    description: `Thales, Telmex, CDMX, CCTV, Face Recognition`,
+    siteUrl: `https://something.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `something`,
     },
   },
   plugins: [
